bf27e5c247fa5181d301832a252638b876189f31
jumagu <jmgg.gutierrez@gmail.com>
2024-08-23 14:51:12 -0500

    chore: Update Home page to include Header and list of documents

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,46 @@
-import { Button } from "@/components/ui/button";
+import Header from "@/components/Header";
+import { SignedIn, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
 
-export default function Home() {
+import AddDocumentBtn from "@/components/AddDocumentBtn";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+export default async function Home() {
+  const clerkUser = await currentUser();
+
+  if (!clerkUser) redirect("/sign-in");
+
+  const documents = [];
+
   return (
-    <Button>Click Me</Button>
+    <main className="home-container">
+      <Header className="sticky left-0 top-0">
+        <div className="flex items-center gap-2 lg:gap-4">
+          Notifications
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </Header>
+
+      {documents.length > 0 ? (
+        <div></div>
+      ) : (
+        <div className="document-list-empty">
+          <Image
+            src="/assets/icons/doc.svg"
+            width={40}
+            height={40}
+            alt="Document"
+            className="mx-auto"
+          />
+          <AddDocumentBtn
+            userId={clerkUser.id}
+            email={clerkUser.emailAddresses[0].emailAddress}
+          />
+        </div>
+      )}
+    </main>
   );
 }
