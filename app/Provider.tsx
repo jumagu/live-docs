a8000dc5cbac5fcf9602376cb923f5330fac5f4c
jumagu <jmgg.gutierrez@gmail.com>
2024-08-23 17:39:12 -0500

    feat: Add resolveUsers handler to LiveblocksProvider for resolving active users data

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -4,12 +4,23 @@ import {
   LiveblocksProvider,
   ClientSideSuspense,
 } from "@liveblocks/react/suspense";
+import type { ResolveUsersArgs } from "@liveblocks/node";
 
 import Loader from "@/components/Loader";
+import { getClerkUsers } from "@/lib/actions/user.actions";
 
 function Provider({ children }: { children: React.ReactNode }) {
+  const resolveUserHandler = async ({ userIds }: ResolveUsersArgs) => {
+    const users = await getClerkUsers({ userIds });
+
+    return users;
+  };
+
   return (
-    <LiveblocksProvider authEndpoint="/api/liveblocks-auth">
+    <LiveblocksProvider
+      authEndpoint="/api/liveblocks-auth"
+      resolveUsers={resolveUserHandler}
+    >
       <ClientSideSuspense fallback={<Loader />}>{children}</ClientSideSuspense>
     </LiveblocksProvider>
   );
