e8c50c52d872e3dcb620aac7b0e6fdabe4eb332e
jumagu <jmgg.gutierrez@gmail.com>
2024-08-22 22:35:13 -0500

    - Add initial configuration (shadcn, tailwind, theme, editor) - Add assets

---
+++ components/Header.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import Image from "next/image";
+
+const Header = ({ children }: HeaderProps) => {
+  return (
+    <div className="header">
+      <Link href="/" className="md:flex-1">
+        <Image
+          src="/assets/icons/logo.svg"
+          width={120}
+          height={32}
+          alt="Logo with name"
+          className="hidden md:block"
+        />
+        <Image
+          src="/assets/icons/logo-icon.svg"
+          width={32}
+          height={32}
+          alt="Logo"
+          className="mr-2 md:hidden"
+        />
+      </Link>
+      {children}
+    </div>
+  );
+};
+
+export default Header;
