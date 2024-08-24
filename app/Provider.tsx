ac5db7a06fad296eb8c3dca25372671731a1dee7
jumagu <jmgg.gutierrez@gmail.com>
2024-08-24 13:12:08 -0500

    feat: mentions

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -1,25 +1,44 @@
 "use client";
 
+import { useUser } from "@clerk/nextjs";
+
 import {
   LiveblocksProvider,
   ClientSideSuspense,
 } from "@liveblocks/react/suspense";
 import type { ResolveUsersArgs } from "@liveblocks/node";
+import type { ResolveMentionSuggestionsArgs } from "@liveblocks/client";
 
 import Loader from "@/components/Loader";
-import { getClerkUsers } from "@/lib/actions/user.actions";
+import { getClerkUsers, getDocumentUsers } from "@/lib/actions/user.actions";
 
 function Provider({ children }: { children: React.ReactNode }) {
-  const resolveUserHandler = async ({ userIds }: ResolveUsersArgs) => {
+  const { user: clerkUser } = useUser();
+
+  const resolveUsers = async ({ userIds }: ResolveUsersArgs) => {
     const users = await getClerkUsers({ userIds });
 
     return users;
   };
 
+  const resolveMentionSuggestions = async ({
+    text,
+    roomId,
+  }: ResolveMentionSuggestionsArgs) => {
+    const roomUsers = await getDocumentUsers({
+      text,
+      roomId,
+      currentUser: clerkUser?.emailAddresses[0].emailAddress!,
+    });
+
+    return roomUsers;
+  };
+
   return (
     <LiveblocksProvider
       authEndpoint="/api/liveblocks-auth"
-      resolveUsers={resolveUserHandler}
+      resolveUsers={resolveUsers}
+      resolveMentionSuggestions={resolveMentionSuggestions}
     >
       <ClientSideSuspense fallback={<Loader />}>{children}</ClientSideSuspense>
     </LiveblocksProvider>
